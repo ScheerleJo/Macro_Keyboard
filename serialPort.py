a4b40ff907e888377fb60fee1b23f0d5960831a5
Josia <scheerle.josia@gmail.com>
2022-03-22 22:02:48 +0100

    Fixed match problem in python

--- serialPort.py
+++ serialPort.py
@@ -3,14 +3,14 @@ import serial
 serialString = "" # Used to hold data coming over UART
 macroLayer = 0
 
-serialPort = serial.Serial(port = "COM5", baudrate=9600, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+serialPort = serial.Serial(port = "COM4", baudrate=9600, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
 
 def keyFunctionLayer(button):
     #print('Utility Layer')
     match button:
-        case 1: 
+        case "1": 
             print("Case 1 was activated")
-        case 2: 
+        case "2": 
             print("Case 2 was activated")
         case "Switch Klicked": 
             print("Switch was pressed")
@@ -26,8 +26,8 @@ while True:
         serialString = serialPort.readline()
 
         # Print the contents of the serial data
-        print(serialString.decode('Ascii'))
-        keyFunctionLayer(serialString.decode('Ascii'))
+        print(serialString.decode('Ascii').strip())
+        keyFunctionLayer(serialString.decode('Ascii').strip())
 
         # Tell the device connected over the serial port that we recevied the data!
         # The b at the beginning is used to indicate bytes!
