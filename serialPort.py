d3cfc26c76de2375c8472f336a706051b318557e
ScheerleJo <scheerle.josia@gmail.com>
2022-03-23 12:40:42 +0100

    playing around with encoder and calling functions in ahk

--- serialPort.py
+++ serialPort.py
@@ -1,19 +1,41 @@
 import serial
+from pynput.keyboard import Key, Controller
+keyboard = Controller()
 
 serialString = "" # Used to hold data coming over UART
-macroLayer = 0
+lastButton = 0;
 
-serialPort = serial.Serial(port = "COM4", baudrate=9600, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+serialPort = serial.Serial(port = "COM5", baudrate=9600, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
 
-def keyFunctionLayer(button):
+def keyFunctionLayer(button:str, lastbutton:int):
+    print(button)
     #print('Utility Layer')
-    match button:
-        case "1": 
-            print("Case 1 was activated")
-        case "2": 
-            print("Case 2 was activated")
-        case "Switch Klicked": 
-            print("Switch was pressed")
+    #match button:
+    #   case "1": 
+    #      print("Case 1 was activated")
+    #   case "2": 
+    #       print("Case 2 was activated")
+    #   case "Switch Klicked": 
+    #       print("Switch was pressed")
+
+    if button == "Switch Klicked":
+        keyboard.press(Key.f20)
+        keyboard.release(Key.f20)
+    elif button == "System successfully booted":
+        print('Ready to go!')
+    elif int(button) > lastbutton:
+        keyboard.press(Key.f19)
+        keyboard.release(Key.f19)
+        print("b: " + str(button) + " l: " + str(lastButton))
+        lastbutton = int(button)
+    
+    elif int(button) < lastbutton:
+        keyboard.press(Key.f18)
+        keyboard.release(Key.f18)
+        print("b: " + str(button) + " l: " + str(lastButton))
+        lastbutton = int(button)
+
+    return lastbutton
 
 
 serialPort.write(b"This is the data sent \r\n");
@@ -26,8 +48,8 @@ while True:
         serialString = serialPort.readline()
 
         # Print the contents of the serial data
-        print(serialString.decode('Ascii').strip())
-        keyFunctionLayer(serialString.decode('Ascii').strip())
+        #print(serialString.decode('Ascii').strip())
+        lastButton = keyFunctionLayer(serialString.decode('Ascii').strip(), lastButton)
 
         # Tell the device connected over the serial port that we recevied the data!
         # The b at the beginning is used to indicate bytes!
---
+++ test.ahk
@@ -0,0 +1,11 @@
+#SingleInstance, force
+
+F20::
+
+    Return
+F19::
+    MouseMove, 10, 0, 0, R
+    Return
+F18::
+    MouseMove, -10, 0, 0, R
+    Return
